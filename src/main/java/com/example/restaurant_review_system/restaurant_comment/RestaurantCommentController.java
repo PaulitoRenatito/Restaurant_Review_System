780adfdc3c910a008c6d830c00b102dc90a71820
GaTalim <gabriel.talim@gmail.com>
2023-11-10 15:19:57 -0300

    adicionando teste

---
+++ src/main/java/com/example/restaurant_review_system/restaurant_comment/RestaurantCommentController.java
@@ -0,0 +1,4 @@
+package com.example.restaurant_review_system.restaurant_comment;
+
+public class RestaurantCommentController {
+}
---
+++ src/main/java/com/example/restaurant_review_system/restaurant_comment/RestaurantCommentRepository.java
@@ -0,0 +1,4 @@
+package com.example.restaurant_review_system.restaurant_comment;
+
+public interface RestaurantCommentRepository {
+}
