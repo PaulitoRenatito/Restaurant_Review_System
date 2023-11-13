589f1b61440c6150d5baa4b2d63c755b3e382c4a
PaulitoRenatito <69298871+PaulitoRenatito@users.noreply.github.com>
2023-11-13 10:18:58 -0300

    Delete Frontend directory

--- Frontend/src/interface/UserData.ts
+++
@@ -1,8 +0,0 @@
-export interface UserData {
-    id?: number,
-    first_name: string,
-    last_name: string,
-    profile_picture: string,
-    email: string,
-    password: string
-}
--- Frontend/src/interface/ValuationData.ts
+++
@@ -1,8 +0,0 @@
-export interface ValuationData {
-    user_id?: number,
-    restaurant_id?: number,
-    rating: number,
-    comment: string,
-    date: Date,
-    hour: string
-}
